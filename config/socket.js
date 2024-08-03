a81b2bd3b4a4d8eb607f03655d4a437c02557296
Kieran-byte <bettesworthkieran1@gmail.com>
2024-08-03 19:40:41 +1200

    websocket config

--- config/socket.js
+++ config/socket.js
@@ -1,19 +1,18 @@
-module.exports = (io) => {
-    io.on('connection', (socket) => {
+module.exports = (wss) => {
+    wss.on('connection', (socket) => {
         console.log('New client connected');
 
         socket.on('playerAction', (actionData) => {
             // Handle player actions and update game state
-            io.emit('update', { /* updated game state */ });
+            wss.emit('update', { /* updated game state */ });
 
 
-
-
-
-
-
-
-
+            // Then you can handle actions and broadcast updates
+            wss.clients.forEach((client) => {
+                if (client.readyState === client.OPEN) {
+                    client.send(JSON.stringify({ /* updated game state */ }));
+                }
+            });
         });
 
         socket.on('disconnect', () => {
